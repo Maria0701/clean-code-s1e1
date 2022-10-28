caa3cbdfbe4e4208883f9c5893fecfc725faba1c
Masha0701 <reflection2008@gmail.com>
2022-10-28 22:02:46 +0300

    refactor: выбор по селектору в js приведены к единому виду

--- app.js
+++ app.js
@@ -8,10 +8,10 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById('new-task');//Add a new task.
-var addButton=document.getElementsByTagName('button')[0];//first button
-var incompleteTaskHolder=document.getElementById('incomplete-tasks');//ul of #incomplete-tasks
-var completedTasksHolder=document.getElementById('completed-tasks');//completed-tasks
+var taskInput=document.querySelector('.add-element__text');//Add a new task.
+var addButton=document.querySelectorAll('.button')[0];//first button
+var incompleteTaskHolder=document.querySelector('#incomplete-tasks');//ul of #incomplete-tasks
+var completedTasksHolder=document.querySelector('#completed-tasks');//completed-tasks
 
 
 //New task list item
